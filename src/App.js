1c0db4e807d099a6ba732e7777c2e53de9afb539
annaleaschroeder <67912360+annaleaschroeder@users.noreply.github.com>
2020-09-03 20:54:38 +0200

    Toggle-Switch working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
-import TransactionInputPage from './components/TransactionInputPage'
+import Toggle from './components/ToggleSwitch'
+
+//import TransactionInputPage from './components/TransactionInputPage'
 
 function App() {
+  const [isToggled, setIsToggled] = useState(false)
   return (
     <div>
       <Header date={new Date()} />
-      <TransactionInputPage />
+      <Toggle
+        id="test-switch"
+        toggled={isToggled}
+        onChange={(event) => setIsToggled(event.target.checked)}
+      />
+      {/* <TransactionInputPage /> */}
     </div>
   )
 }
--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import styled from 'styled-components/macro'
-import './styles.css'
 
-export default function Toggle({ toggled, onClick }) {
+export default function ToggleSwitch({ toggled, onChange }) {
   return (
-    <ToggleContainer onClick={onClick} toggled={toggled}>
-      <ToggleNotch className="notch"></ToggleNotch>
-    </ToggleContainer>
+    <div>
+      <Switch id="test-switch" toggled={toggled} onChange={onChange} />
+    </div>
+  )
+}
+
+const Switch = ({ id, toggled, onChange }) => {
+  console.log(toggled)
+  return (
+    <>
+      <SwitchInput
+        className="switch-checkbox"
+        id={id}
+        type="checkbox"
+        checked={toggled}
+        onChange={onChange}
+      />
+      <SwitchLabel toggled={toggled} className="switch-label" htmlFor={id}>
+        <SwitchButton className="switch-button" />
+      </SwitchLabel>
+    </>
   )
 }
 
@@ -15,28 +32,82 @@ const toggleColors = {
   false: 'red', //entspricht spending
 }
 
-const ToggleContainer = styled.div`
-  height: 50px;
-  width: 160px;
-  border-radius: 20px;
-  margin: auto;
-  background: ${(props) => toggleColors[props.toggled]};
-  position: relative;
-  cursor: pointer;
+const SwitchInput = styled.input`
+  height: 0;
+  width: 0;
+  visibility: hidden;
 `
 
-const ToggleNotch = styled.div`
+const SwitchLabel = styled.label`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+  width: 100px;
   height: 40px;
-  width: 60px;
-  border-radius: 20%;
-  background: whitesmoke;
-  position: absolute;
-  top: 5px;
-  left: 5px;
-  box-shadow: 0 0 5px whitesmoke;
-  z-index: 1;
-  transition: all 0.3s ease;
+  border-radius: 100px;
+  border: 2px solid gray;
+  position: relative;
+  background: ${(props) => toggleColors[props.toggled]};
+  transition: background-color 0.2s;
 `
+
+const SwitchButton = styled.span`
+  content: "";
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  width: 36px;
+  height: 36px;
+  border-radius: 45px;
+  transition: 0.2s;
+  background: grey;
+  box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.29);
+  ${SwitchInput}:checked + ${SwitchLabel} & {
+    left: calc(100% - 2px);
+    transform: translateX(-100%);
+  }
+
+  ${SwitchLabel}:active & {
+    width: 45px;
+  }
+`
+
+// export default function Toggle({ toggled, onClick }) {
+//   return (
+//     <ToggleContainer onClick={onClick} toggled={toggled}>
+//       <ToggleNotch className="notch"></ToggleNotch>
+//     </ToggleContainer>
+//   )
+// }
+
+// const toggleColors = {
+//   true: 'green', //entspricht income
+//   false: 'red', //entspricht spending
+// }
+
+// const ToggleContainer = styled.div`
+//   height: 50px;
+//   width: 160px;
+//   border-radius: 20px;
+//   margin: auto;
+//   background: ${(props) => toggleColors[props.toggled]};
+//   position: relative;
+//   cursor: pointer;
+// `
+
+// const ToggleNotch = styled.div`
+//   height: 40px;
+//   width: 60px;
+//   border-radius: 20%;
+//   background: whitesmoke;
+//   position: absolute;
+//   top: 5px;
+//   left: 5px;
+//   box-shadow: 0 0 5px whitesmoke;
+//   z-index: 1;
+//   transition: all 0.3s ease;
+// `
 
 /* // export default function ToggleSwitch({ text, toggled, onClick }) {
   //   return (
