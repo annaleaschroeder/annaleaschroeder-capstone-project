00617fe6b56dd3b7b84c131d7d487be9dce67662
annaleaschroeder <67912360+annaleaschroeder@users.noreply.github.com>
2020-09-06 10:49:21 +0200

    Add styling and color-code to toggle-switch

--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -9,18 +9,27 @@ ToggleSwitch.propTypes = {
 
 export default function ToggleSwitch({ selected, toggleSelected }) {
   return (
-    <ToggleContainer onClick={toggleSelected}>
-      <DialogueButton className={selected ? '' : 'disabled'}>
-        {selected ? 'Spending' : 'Income'}
-      </DialogueButton>
-    </ToggleContainer>
+    <ToggleWrapper>
+      <ToggleContainer onClick={toggleSelected}>
+        <DialogueButton className={selected ? '' : 'disabled'}>
+          {selected ? 'Income' : 'Spending'}
+        </DialogueButton>
+      </ToggleContainer>
+    </ToggleWrapper>
   )
 }
 
+//org. width 70px, height: 32px
+
+const ToggleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
 const ToggleContainer = styled.div`
   position: relative;
-  width: 70px;
-  height: 32px;
+  width: 124px;
+  height: 36px;
+  margin: 10px;
   border-radius: 3px;
   padding: 2px;
   background-color: #c4c4c4;
@@ -37,103 +46,20 @@ const DialogueButton = styled.div`
   font-weight: bold;
   line-height: 16px;
   color: white;
-  background-color: #002b49;
+  background-color: #158467;
   padding: 8px 12px;
   border-radius: 18px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  min-width: 46px;
-  width: 38px;
+  min-width: 50px;
+  /* width: 38px; */
   min-width: unset;
   border-radius: 3px;
-  left: 34px;
+  left: 50px;
   transition: all 0.3s cubic-bezier(0.55, 0.06, 0.68, 0.19);
   cursor: pointer;
 
   &.disabled {
-    background-color: #707070;
+    background-color: #d54062;
     left: 2px;
   }
 `
-
-// export default function ToggleSwitch({ toggled, onChange }) {
-//   return <Switch id="test-switch" toggled={toggled} onChange={onChange} />
-// }
-
-// const Switch = ({ id, toggled, onChange }) => {
-//   return (
-//     <>
-//       <SwitchContainer>
-//         <SpendingRed>Spending</SpendingRed>
-//         <HiddenSwitchInput
-//           className="switch-checkbox"
-//           id={id}
-//           type="checkbox"
-//           checked={toggled}
-//           onChange={onChange}
-//         />
-//         <SwitchLabel toggled={toggled} className="switch-label" htmlFor={id}>
-//           <SwitchButton className="switch-button"></SwitchButton>
-//         </SwitchLabel>
-//         <IncomeGreen>Income</IncomeGreen>
-//       </SwitchContainer>
-//     </>
-//   )
-// }
-
-// const SwitchContainer = styled.div`
-//   display: flex;
-//   justify-content: center;
-// `
-// const SpendingRed = styled.div`
-//   color: red;
-//   font-weight: bold;
-// `
-// const IncomeGreen = styled.div`
-//   color: green;
-//   font-weight: bold;
-// `
-// const toggleColors = {
-//   true: '#36ffa1', //refers to income
-//   false: '#ff7a78', //refers to spending
-// }
-
-// const HiddenSwitchInput = styled.input`
-//   height: 0;
-//   width: 0;
-//   visibility: hidden;
-// `
-
-// const SwitchLabel = styled.label`
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   cursor: pointer;
-//   width: 100px;
-//   height: 40px;
-//   border-radius: 100px;
-//   border: 2px solid gray;
-//   position: relative;
-//   background: ${(props) => toggleColors[props.toggled]};
-//   transition: background-color 0.2s;
-// `
-
-// const SwitchButton = styled.span`
-//   content: "";
-//   position: absolute;
-//   top: 2px;
-//   left: 2px;
-//   width: 36px;
-//   height: 36px;
-//   border-radius: 45px;
-//   transition: 0.2s;
-//   background: grey;
-//   box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.29);
-//   ${HiddenSwitchInput}:checked + ${SwitchLabel} & {
-//     left: calc(100% - 2px);
-//     transform: translateX(-100%);
-//   }
-
-//   ${SwitchLabel}:active & {
-//     width: 45px;
-//   }
-// `
