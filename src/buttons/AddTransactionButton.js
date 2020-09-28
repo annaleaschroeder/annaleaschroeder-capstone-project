96d9f3d7f5c42529c549c4ae9a8d2259bdf49781
annaleaschroeder <67912360+annaleaschroeder@users.noreply.github.com>
2020-09-28 18:11:45 +0200

    Fix styling

--- src/buttons/AddTransactionButton.js
+++ src/buttons/AddTransactionButton.js
@@ -4,20 +4,18 @@ import styled from 'styled-components/macro'
 export default function AddTransactionButton() {
   return (
     <ButtonContainer>
-      <AddTrxButton>
-        <AddTrxRoute href="/add-new-transaction">
-          <AddImage
-            xmlns="http://www.w3.org/2000/svg"
-            height="40"
-            viewBox="0 0 24 24"
-            width="40"
-            fill="white"
-          >
-            <path d="M0 0h24v24H0V0z" fill="none" />
-            <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
-          </AddImage>
-        </AddTrxRoute>
-      </AddTrxButton>
+      <AddTrxRoute href="/add-new-transaction">
+        <AddImage
+          xmlns="http://www.w3.org/2000/svg"
+          height="40"
+          viewBox="0 0 24 24"
+          width="40"
+          fill="white"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+        </AddImage>
+      </AddTrxRoute>
     </ButtonContainer>
   )
 }
@@ -33,7 +31,8 @@ const ButtonContainer = styled.div`
   justify-items: center;
 `
 
-const AddTrxButton = styled.button`
+const AddTrxRoute = styled.a`
+  text-decoration: none;
   display: flex;
   border: none;
   border-radius: 50px;
@@ -44,11 +43,6 @@ const AddTrxButton = styled.button`
   position: fixed;
   z-index: 10;
   bottom: 40px;
-`
-const AddTrxRoute = styled.a`
-  text-decoration: none;
-  height: auto;
-  width: auto;
 `
 
 const AddImage = styled.svg`
